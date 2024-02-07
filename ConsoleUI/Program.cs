c044d39978513ea631e4ce728254989c1ec0a291
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 13:50:16 -0500

    Create Code
    We created an array of 3 names. Asked the user which number to select. When the user gives us a number, return that name.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,23 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			string[] names = new string[] { "Tim", "Sue", "Bob" };
+
+			Console.Write("Pick a number from 1 to 3: ");
+			string numbertext = Console.ReadLine();
+
+			bool isvalidnumber = int.TryParse(numbertext, out int number);
+
+			if ( !isvalidnumber || number < 1 || number > 3 )
+			{
+				Console.WriteLine("That is not a valid number.");
+				_ = Console.ReadLine();
+				return;
+			}
+
+			Console.WriteLine($"The name in position {number} is {names[number - 1]}.");
+
+			_ = Console.ReadLine();
 		}
 	}
 }
